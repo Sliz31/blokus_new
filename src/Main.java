2668b68c3bb09e5c3b727f2e2d38ae30be72678a
Sliz31 <petroship131@gmail.com>
2026-05-04 21:38:51 +0300

    fix UI for mac

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import Logic.Game;
+import javax.swing.UIManager;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello and welcome to Blokus!");
+    try {
+        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
 
+    System.out.println("Hello and welcome to Blokus!");
     // Initialize the game
     Game game = new Game();
-
   }
 }
